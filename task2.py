d5351120472e535832cbe229dc98c6033b5caff3
kalpataru123 <sahookalpataru683@gmail.com>
2025-06-15 23:44:06 +0530

    Add new Assignment

---
+++ task2.py
@@ -0,0 +1,8 @@
+import math;
+n=int(input("Enter a number:"));
+sq_root=math.sqrt(n);
+print("Squqre root:",sq_root);
+log_num=math.log(n,10);
+print("logarithm:",log_num);
+sin_val=math.sin(n);
+print("sine:",sin_val);
